2467475347f8f08f38480d00d1f5fecd63db041c
Kalas134 <dk6249@gmail.com>
2026-05-29 15:26:35 +0900

    added Ex_DataType.java
    added Ex_DataType.java

---
+++ proj_0529/src/com/kdj/datatype/Ex_DataType.java
@@ -0,0 +1,16 @@
+package com.kdj.datatype;
+
+public class Ex_DataType {
+
+	public static void main(String[] args) {
+		int myNum = 5;
+		float myFloatNum =5.99f;
+		char meLetter = 'D';
+		boolean myBool = true;
+		String myText = "Hello";
+		
+		System.out.println("자동 타입 캐스팅\n ------------ \n byte-> short-> char-> int -> long-> float-> double");
+
+	}
+
+}
