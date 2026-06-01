bc0b84238089e1bb48e3bb657adb58e9c62c7fd5
Kalas134 <dk6249@gmail.com>
2026-06-01 18:07:25 +0900

    update(2606011807)
    update(2606011807)

--- proj_0601/src/com/kdj/Car.java
+++ proj_0601/src/com/kdj/Car.java
@@ -2,4 +2,5 @@ package com.kdj;
 
 public class Car {
 	String brand = "Hyundai";
+	String engine = "휘발유";
 }
--- proj_0601/src/com/kdj/Ex04_Car.java
+++ proj_0601/src/com/kdj/Ex04_Car.java
@@ -5,6 +5,7 @@ public class Ex04_Car {
 	public static void main(String[] args) {
 		Car car = new Car();
 		System.out.println(car.brand);
+		System.out.println(car.engine);
 	}
 
 }
