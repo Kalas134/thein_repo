73341c850f7130769d9e6881b92b2ccbfe0ff013
Kalas134 <dk6249@gmail.com>
2026-06-04 16:13:17 +0900

    과제 업데이트
    과제 업데이트

---
+++ proj_EX/src/com/kdj/exp/Ex01_009.java
@@ -0,0 +1,15 @@
+package com.kdj.exp;
+
+public class Ex01_009 {
+
+	public static void main(String[] args) {
+//		배열 연산자는 다음과 같이 정의 된다.
+		int[] numbers = {10, 20, 30};
+		
+		System.out.println("첫번째: " + numbers[0]);
+		System.out.println("두번째: " + numbers[1]);
+		System.out.println("세번째: " + numbers[2]);
+		System.out.println("배열 길이: " + numbers.length);
+	}
+
+}
