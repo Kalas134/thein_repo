e668b0bf9d477c5b9180d070317d65c5186f5274
Kalas134 <dk6249@gmail.com>
2026-06-05 09:39:44 +0900

    예제 숙제 완성
    예제 숙제 완성

---
+++ proj_EX/src/com/kdj/exp_1/Ex02_003.java
@@ -0,0 +1,16 @@
+package com.kdj.exp_1;
+
+public class Ex02_003 {
+
+	public static void main(String[] args) {
+		System.out.println("최댓값: " + Math.max(10, 20));
+		System.out.println("최솟값: " + Math.min(10, 20));
+		System.out.println("절댓값: " + Math.abs(-15));
+		System.out.println("2의 3제곱: " + Math.pow(2, 3));
+		System.out.println("64의 제곱근: " + Math.sqrt(64));
+		System.out.println("반올림: " + Math.round(3.7));
+		System.out.println("올림: " + Math.ceil(3.1));
+		System.out.println("내림: " + Math.floor(3.9));
+	}
+
+}
